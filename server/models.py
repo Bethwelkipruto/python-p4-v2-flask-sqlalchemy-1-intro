b48e70068f10aed340d603c138da365c78351142
bethwelkipruto <bethwel.c7@gmail.com>
2025-09-16 03:01:29 +0300

    Add Review model with Customer and Item relationships

--- server/models.py
+++ server/models.py
@@ -16,3 +16,28 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+class Customer(db.Model):
+    __tablename__ = 'customers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    reviews = db.relationship('Review', back_populates='customer')
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    
+    reviews = db.relationship('Review', back_populates='item')
+
+class Review(db.Model):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    comment = db.Column(db.String)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'))
+
+    customer = db.relationship('Customer', back_populates='reviews')
+    item = db.relationship('Item', back_populates='reviews')
